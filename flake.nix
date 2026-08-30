cf36ed8185e2c95c8d4802eca49ce46568e1230f
khalil hadji <h-adji_tech@proton.me>
2026-08-30 14:17:06 +0200

    flake direnv setup

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Pi extensions dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in
+    {
+      devShells.x86_64-linux = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ ];
+          shellHook = "";
+        };
+        # alternative shell profile : nix develop .#special
+        special = pkgs.mkShell { };
+      };
+    };
+}
